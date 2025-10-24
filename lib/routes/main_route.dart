825dcf3ec2bad351a9a753c71601c330f046615c
Wanderer2414 <huynguyenlequoc13@gmail.com>
2025-10-24 14:20:21 +0700

    feat: base home screen ui

--- lib/routes/main_route.dart
+++ lib/routes/main_route.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:vietnambeyondthehorizon/presentation/screens/home_screen.dart';
 import 'package:vietnambeyondthehorizon/presentation/screens/information_register_screen.dart';
 
 class MainRoute {
   static const String home = "/";
   static Map<String, WidgetBuilder> routes = {
-    home: (context) => InformationRegisterStation(),
+    "register": (context) => InformationRegisterScreen(),
+    home: (context) => HomeStation(),
   };
 }
