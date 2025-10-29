bf0f17833bc9289a96cce8609b013a20d4d0870f
nguyenlequochuy <huynguyenlequoc13@gmail.com>
2025-10-29 14:29:19 +0700

    feat: login screen design

--- lib/routes/main_route.dart
+++ lib/routes/main_route.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:vietnambeyondthehorizon/osm_page.dart';
+import 'package:vietnambeyondthehorizon/presentation/screens/account_login.dart';
+import 'package:vietnambeyondthehorizon/presentation/screens/account_register.dart';
 import 'package:vietnambeyondthehorizon/presentation/screens/information_register_screen.dart';
 
 class MainRoute {
   static const String home = "/";
   static Map<String, WidgetBuilder> routes = {
-    "register": (context) => InformationRegisterScreen(),
-    home: (context) => OpenStreetMapScreen(),
+    home: (context) => AccountLoginScreen(),
   };
 }
